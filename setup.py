a9f5ac75f77f8ebc3804f14fd666f5b63b387df5
piotr <piotr329@gmail.com>
2020-07-28 21:04:57 +0200

    Project draft

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pathfinder",
+    version="0.1",
+    author="Piotr Winkler",
+    description="dev-test-python",
+    keywords="python",
+    packages=['pathfinder'],
+)
